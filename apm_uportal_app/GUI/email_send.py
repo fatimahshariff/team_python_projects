7253afa136cf923c6ee1f1bb131be26660b8d5d5
Aamir Hussain <aamir222686@yahoo.com>
2019-11-26 17:40:22 +0530

    Update email_send.py

--- apm_uportal_app/GUI/email_send.py
+++ apm_uportal_app/GUI/email_send.py
@@ -4,22 +4,26 @@ from email.message import EmailMessage
 username = 'apmadmin@autopartmine.com'
 password = 'apmadmin@0011'
 
-contacts = ['fathimamasood48@gmail.com', 'fahadbaig663@gmail.com']
+contacts = ['fahadbaig663@gmail.com']
 
 msg = EmailMessage()
 msg['Subject'] = 'This is a test'
 msg['From'] = username
 msg['To'] = contacts
 
+name = 'Aamir'
 msg.set_content('Plain Text Message')
-msg.add_alternative("""\
+
+html_msg = """\
 <!DOCTYPE html>
 <html>
     <body>
-        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+        <h1 style="color:SlateGray;">This is an HTML Email! {name}</h1>
     </body>
 </html>
-""", subtype='html')
+""".format(name=name)
+
+msg.add_alternative(html_msg, subtype='html')
 
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
